824b630a61d7dca8309211e5042c558ade5e9d1f
Abdifatah <mursalabdifatah17@gmail.com>
2025-12-02 09:35:18 +0300

    python-p3-translating-from-orms-to-sqlalchemy-lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,45 @@
 from models import Dog
 
-def create_table(base):
-    pass
+# Create the table using the Base and engine
+def create_table(Base, engine):
+    Base.metadata.create_all(engine)
 
+# Save a Dog instance to the database
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return dog
 
+# Create a new Dog and save it
+def create(session, name, breed):
+    dog = Dog(name=name, breed=breed)
+    return save(session, dog)
+
+# Get all Dog instances
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
+# Find a dog by name
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
+# Find a dog by id
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
+# Find a dog by name and breed
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+# Find or create a dog by name and breed
+def find_or_create_by(session, name, breed):
+    dog = find_by_name_and_breed(session, name, breed)
+    if dog is None:
+        dog = create(session, name, breed)
+    return dog
+
+# Update a dog's breed
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
+    session.commit()
+    return dog
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python3
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-from sqlalchemy import (Column, String, Integer)
-from sqlalchemy.ext.declarative import declarative_base
+engine = create_engine('sqlite:///dogs.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
 
 class Dog(Base):
-    __tablename__ = 'dogs'
+    __tablename__ = "dogs"
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
+
+    def __repr__(self):
+        return f"<Dog id={self.id} name={self.name} breed={self.breed}>"
